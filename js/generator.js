7cf9a79c146b7c7133802143fdc6924b766388c8
Conor <55244147+conor-timmis@users.noreply.github.com>
2024-05-01 19:41:20 +0100

    Create Generate Function

---
+++ js/generator.js
@@ -0,0 +1,10 @@
+function generate() {
+    var length = document.getElementById('length').value;
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    document.getElementById('password').value = result;
+}
